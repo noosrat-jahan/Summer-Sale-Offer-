85631bde23f65e4a3da35d68537c4e9a82f1d417
noosrat-jahan <noosratmeem@gmail.com>
2024-09-27 09:51:49 +0600

    an website design practiced named summer sale offer. it contain some functionalities implemented by js DOM and design is made with tailwind css

---
+++ main.js
@@ -0,0 +1,57 @@
+function AddEventInItemById(id){    
+    document.getElementById(id).addEventListener('click', function(){
+        const productDiv = document.getElementById(id)
+        const productName = productDiv.querySelector('h2').innerText
+        const productPrice = parseFloat(productDiv.querySelector('span').innerText)
+        
+        console.log(productName);
+        console.log(productPrice);
+
+        let totalPrice = parseFloat(document.getElementById('total-price').innerText)
+        totalPrice += productPrice
+        document.getElementById('total-price').innerText = totalPrice
+
+
+        let finalTotal = totalPrice
+        if(totalPrice >= 200){
+            const discountedPrice = (totalPrice * 20) / 100
+            document.getElementById('discount-price').innerText = discountedPrice
+            finalTotal = totalPrice - discountedPrice
+        }
+        document.getElementById('final-total').innerText = finalTotal
+
+        const cartContainer = document.createElement('div')
+        cartContainer.innerHTML = `
+            <div>
+                <p class="mb-2 font-semibold text-lg">${productName}</p>
+            </div>
+        `
+        const totalCountDiv = document.getElementById('total-count-div')
+        // document.getElementById('purchased-product-list').insertBefore(cartContainer, totalCountDiv)
+        document.getElementById('purchased-product-list').appendChild(cartContainer)
+    })
+}
+
+AddEventInItemById('product-1')
+AddEventInItemById('product-2')
+AddEventInItemById('product-3')
+AddEventInItemById('product-4')
+AddEventInItemById('product-5')
+AddEventInItemById('product-6')
+AddEventInItemById('product-7')
+AddEventInItemById('product-8')
+AddEventInItemById('product-9')
+
+
+document.getElementById('purchase=btn').addEventListener('click', function(){
+
+    my_modal_1.showModal()
+    document.getElementById('purchased-product-list').innerHTML = ''
+    const totalPrice = document.getElementById('total-price')
+    const discountPrice = document.getElementById('discount-price')
+    const finalTotalPrice = document.getElementById('final-total')
+    totalPrice.innerText = '00'
+    discountPrice.innerText = '00'
+    finalTotalPrice.innerText = '00'
+
+})
